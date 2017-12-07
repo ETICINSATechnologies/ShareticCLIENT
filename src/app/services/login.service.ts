cea6e7f5e407a7fb811566c8427abe94b4349e56
Vincent Redouté <vincent.redoute@live.fr>
2017-12-07 22:58:01 +0100

    getting rid of a comment

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -29,7 +29,7 @@ export class LoginService {
   }
 
   private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
+    console.error('An error occurred', error); 
     return Promise.reject(error.message || error);
   }
 
