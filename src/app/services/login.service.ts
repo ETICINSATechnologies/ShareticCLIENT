e4574bf104c3a62895eb8a7d2e802b5101d2c3cb
Vincent Redouté <vincent.redoute@live.fr>
2017-12-07 22:59:01 +0100

    no whitespace

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -29,7 +29,7 @@ export class LoginService {
   }
 
   private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); 
+    console.error('An error occurred', error);
     return Promise.reject(error.message || error);
   }
 
