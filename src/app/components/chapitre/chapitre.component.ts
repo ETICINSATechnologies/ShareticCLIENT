4428b414c92110702e72664c1f2808d899faee26
chris23insa <christophe.etienne@insa-lyon.fr>
2017-12-29 21:09:26 +0100

    petiteModifChapetForma

--- src/app/components/chapitre/chapitre.component.ts
+++ src/app/components/chapitre/chapitre.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {FormationService} from '../../services/formation.service';
 import {Chapter} from '../../entities/Chapter';
+import {Formation} from '../../entities/Formation';
 import {AuthService} from '../../services/auth.service';
 import { Location } from '@angular/common';
 
@@ -13,21 +14,32 @@ import { Location } from '@angular/common';
 export class ChapitreComponent implements OnInit {
 
     chapter: Chapter[];
+    formations: Formation[];
+    formationName: string;
+    formationIcon: string;
 
     constructor(private formationService: FormationService, private authService: AuthService, private route: ActivatedRoute,
                 private location: Location) { }
 
     ngOnInit() {
         console.log(this.authService.isLogged());
-        this.getChapter();
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.getChapter(id);
+        this.getListFormations(id);
+        this.formationName = this.formations[id].name;
+        this.formationIcon = this.formations[id].icon;
     }
 
-    getChapter(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
+    getChapter(id: number): void {
         // this.formationService.getChapter(id).subscribe(chapter => this.chapter = chapter);
     }
 
+    getListFormations(id: number): void {
+        this.formationService.getListFormations().subscribe(formations => this.formations = formations);
+    }
+
     validerChapitre(): void {
+        this.location.back();
     }
 
     goBack(): void {
--- src/app/components/formation/formation.component.ts
+++ src/app/components/formation/formation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {FormationService} from '../../services/formation.service';
 import {FormationDetails} from '../../entities/formationDetails';
+import {Formation} from '../../entities/formation';
 import {AuthService} from '../../services/auth.service';
 import { Location } from '@angular/common';
 
@@ -12,19 +13,30 @@ import { Location } from '@angular/common';
 })
 export class FormationComponent implements OnInit {
     formationDetails: FormationDetails[];
+    formations: Formation[];
+    formationName: string;
+    formationIcon: string;
 
   constructor(private formationService: FormationService, private authService: AuthService, private route: ActivatedRoute,
               private location: Location) { }
 
     ngOnInit() {
         console.log(this.authService.isLogged());
-        this.getFormation();
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.getFormation(id);
+        this.getListFormations(id);
+        this.formationName = this.formations[id].name;
+        this.formationIcon = this.formations[id].icon;
     }
 
-    getFormation(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
-        // this.formationService.getFormation(id).subscribe(formation => this.formationDetails = formationDetails);
+    getFormation(id: number): void {
+        // this.formationService.getFormation(id).subscribe(formationDetails => this.formationDetails = formationDetails);
     }
+
+    getListFormations(id: number): void {
+        this.formationService.getListFormations().subscribe(formations => this.formations = formations);
+    }
+
     addNewChapter(): void {
     }
 
