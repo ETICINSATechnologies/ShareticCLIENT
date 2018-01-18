9c739df9671661ce47c588e9aba501cd45239447
Vincent Redouté <vincent.redoute@live.fr>
2018-01-18 14:17:05 +0100

    drag and drop with fruits and vegetables

--- src/app/components/add-formation/add-formation.component.ts
+++ src/app/components/add-formation/add-formation.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFormationComponent implements OnInit {
 
+  vegetables = [
+    {name: 'Carrot', type: 'vegetable'},
+    {name: 'Onion', type: 'vegetable'},
+    {name: 'Potato', type: 'vegetable'},
+    {name: 'Capsicum', type: 'vegetable'}];
+
+  fruits = [
+    {name: 'Apple', type: 'fruit'},
+    {name: 'Orange', type: 'fruit'},
+    {name: 'Mango', type: 'fruit'},
+    {name: 'Banana', type: 'fruit'}];
+
+  droppedFruits = [];
+  droppedVegetables = [];
+  droppedItems = [];
+  fruitDropEnabled = true;
+  dragEnabled = true;
+
+  onFruitDrop(e: any) {
+    this.droppedFruits.push(e.dragData);
+    this.removeItem(e.dragData, this.fruits);
+  }
+
+  onVegetableDrop(e: any) {
+    this.droppedVegetables.push(e.dragData);
+    this.removeItem(e.dragData, this.vegetables);
+  }
+
+  onAnyDrop(e: any) {
+    this.droppedItems.push(e.dragData);
+
+    if (e.dragData.type === 'vegetable') {
+      this.removeItem(e.dragData, this.vegetables);
+    } else {
+      this.removeItem(e.dragData, this.fruits);
+    }
+  }
+
+  removeItem(item: any, list: Array<any>) {
+    const index = list.map(function (e) {
+      return e.name;
+    }).indexOf(item.name);
+    list.splice(index, 1);
+  }
+
   constructor() { }
 
   ngOnInit() {
