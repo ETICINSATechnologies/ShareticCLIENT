cc9501a546863ae97f6634b527ba32ee7c8107ab
Vincent Redouté <vincent.redoute@live.fr>
2018-01-29 10:13:27 +0100

    Rename everything to be chapters

--- src/app/components/add-formation/add-formation.component.ts
+++ src/app/components/add-formation/add-formation.component.ts
@@ -7,41 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFormationComponent implements OnInit {
 
-  vegetables = [
-    {name: 'Carrot', type: 'vegetable'},
-    {name: 'Onion', type: 'vegetable'},
-    {name: 'Potato', type: 'vegetable'},
-    {name: 'Capsicum', type: 'vegetable'}];
+  chapters = [
+    {name: '1', type: 'chapter'},
+    {name: '2', type: 'chapter'},
+    {name: '3', type: 'chapter'},
+    {name: '4', type: 'chapter'}];
 
-  fruits = [
-    {name: 'Apple', type: 'fruit'},
-    {name: 'Orange', type: 'fruit'},
-    {name: 'Mango', type: 'fruit'},
-    {name: 'Banana', type: 'fruit'}];
-
-  droppedFruits = [];
-  droppedVegetables = [];
+  droppedChapters = [];
   droppedItems = [];
-  fruitDropEnabled = true;
   dragEnabled = true;
 
-  onFruitDrop(e: any) {
-    this.droppedFruits.push(e.dragData);
-    this.removeItem(e.dragData, this.fruits);
-  }
-
-  onVegetableDrop(e: any) {
-    this.droppedVegetables.push(e.dragData);
-    this.removeItem(e.dragData, this.vegetables);
+  onChapterDrop(e: any) {
+    this.droppedChapters.push(e.dragData);
+    this.removeItem(e.dragData, this.chapters);
   }
 
   onAnyDrop(e: any) {
     this.droppedItems.push(e.dragData);
 
-    if (e.dragData.type === 'vegetable') {
-      this.removeItem(e.dragData, this.vegetables);
-    } else {
-      this.removeItem(e.dragData, this.fruits);
+    if (e.dragData.type === 'chapter') {
+      this.removeItem(e.dragData, this.chapters);
     }
   }
 
