68b2f707b9eabe88ae377459340323beee348a73
Vincent Redouté <vincent.redoute@live.fr>
2018-01-30 21:05:04 +0100

    fixing urls

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export const API_SERVER: any = {
   url: 'http://127.0.0.1:8000/api/',
-  formation: 'http://localhost:8000/api/formation',
-  formations: 'http://localhost:8000/api/formations',
+  formation: 'http://127.0.0.1:8000/api/formation',
+  formations: 'http://127.0.0.1:8000/api/formations',
 };
