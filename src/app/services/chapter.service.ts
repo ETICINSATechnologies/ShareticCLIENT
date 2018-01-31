1da759a56c1acc001ce36d859dd9b738a1eb615b
Vincent Redouté <vincent.redoute@live.fr>
2018-01-31 16:55:23 +0100

    getChapter by id

--- src/app/services/chapter.service.ts
+++ src/app/services/chapter.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+import 'rxjs/add/operator/map';
+
+import { Chapter } from '../entities/chapter';
+import { API_SERVER } from '../app.constants';
+import {Formation} from "../entities/formation";
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class ChapterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * GET a chapter from the server.
+   * @param {number} id
+   * @returns {Observable<Chapter[]>}
+   */
+  getChapter(id: number): Observable<Chapter> {
+    const url = `${API_SERVER.chapter}/${id}`;
+    return this.http.get<Chapter>(url)
+      .map(result => result['res'])
+      .pipe(catchError(this.handleError<Formation>(`getChapter id=${id}`)));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
 }
