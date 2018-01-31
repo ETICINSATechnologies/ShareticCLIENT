598fd210fb46895af27ac96035517adee8491790
Vincent Redouté <vincent.redoute@live.fr>
2018-01-31 16:55:09 +0100

    deleting useless content

--- src/app/services/formation.service.ts
+++ src/app/services/formation.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of';
 import 'rxjs/add/operator/map';
