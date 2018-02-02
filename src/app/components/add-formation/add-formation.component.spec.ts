99a9d857bd44a365cf5681cf72c294ae5f1e5db9
Vincent Redouté <vincent.redoute@live.fr>
2018-02-02 15:07:26 +0100

    passing tests

--- src/app/components/add-formation/add-formation.component.spec.ts
+++ src/app/components/add-formation/add-formation.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddFormationComponent } from './add-formation.component';
+import {FormationService} from '../../services/formation.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AddFormationComponent', () => {
   let component: AddFormationComponent;
@@ -8,7 +11,9 @@ describe('AddFormationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddFormationComponent ]
+      declarations: [ AddFormationComponent ],
+      providers: [FormationService],
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   }));
