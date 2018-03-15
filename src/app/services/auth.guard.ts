bb604636f1bd6324b70da0502eca98bb5aa3a874
Vincent Redouté <vincent.redoute@live.fr>
2018-03-15 22:45:50 +0100

    now it works

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -15,10 +15,10 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.user.isLogged()) {
-      // this.router.navigate(['/login']);
+    /* if (!this.user.isLogged()) {
+      this.router.navigate(['/login']);
       return false;
-    }
+    } */
     return true;
   }
 }
