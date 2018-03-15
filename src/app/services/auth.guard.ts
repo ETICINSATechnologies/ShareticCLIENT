de2ea686cd4e29614db4c99a5d0fcc92f547f557
Vincent Redouté <vincent.redoute@live.fr>
2018-03-15 21:43:32 +0100

    disable navigation to login

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.user.isLogged()) {
-      this.router.navigate(['/login']);
+      // this.router.navigate(['/login']);
       return false;
     }
     return true;
