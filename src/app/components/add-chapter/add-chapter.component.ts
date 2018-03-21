9d6d80094db813145303035d81ca296eccd72a63
Vincent Redouté <vincent.redoute@live.fr>
2018-03-21 17:40:42 +0100

    can edit name and desc in add-chapter

--- src/app/components/add-chapter/add-chapter.component.ts
+++ src/app/components/add-chapter/add-chapter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Chapter} from '../../entities/chapter';
+import {ChapterService} from '../../services/chapter.service';
 
 @Component({
   selector: 'app-add-chapter',
@@ -8,13 +10,22 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class AddChapterComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  chapter: Chapter;
+
+  constructor(private route: ActivatedRoute, private chapterService: ChapterService) { }
 
   ngOnInit() {
     const idFormation = +this.route.snapshot.paramMap.get('idFormation');
     const idChapter = +this.route.snapshot.paramMap.get('idChapter');
     console.log('formation:' + idFormation);
     console.log('chapter:' + idChapter);
+    this.getChapter(idChapter);
+  }
+
+  getChapter(id: number): void {
+    this.chapterService.getChapter(id).subscribe(chapter => {
+      this.chapter = chapter;
+    });
   }
 
 }
