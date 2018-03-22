550bfbca71c704a07acdc61f0c5438dbdbb433bd
Vincent Redouté <vincent.redoute@live.fr>
2018-03-22 10:54:22 +0100

    save chapter and go back

--- src/app/components/add-chapter/add-chapter.component.ts
+++ src/app/components/add-chapter/add-chapter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Chapter} from '../../entities/chapter';
 import {ChapterService} from '../../services/chapter.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-add-chapter',
@@ -12,7 +13,8 @@ export class AddChapterComponent implements OnInit {
 
   chapter: Chapter;
 
-  constructor(private route: ActivatedRoute, private chapterService: ChapterService) { }
+  constructor(private route: ActivatedRoute, private chapterService: ChapterService,
+              private location: Location ) { }
 
   ngOnInit() {
     const idFormation = +this.route.snapshot.paramMap.get('idFormation');
@@ -26,6 +28,16 @@ export class AddChapterComponent implements OnInit {
     this.chapterService.getChapter(id).subscribe(chapter => {
       this.chapter = chapter;
     });
+  }
+
+  saveChapter(): void {
+    this.chapterService.updateChapter(this.chapter).subscribe(c => {
+      this.chapter = c;
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
